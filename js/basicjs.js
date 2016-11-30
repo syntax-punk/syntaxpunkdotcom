7822f9dd2a3acebe6c1e02607207582326231853
voogieJames <vugar.abdulzadeh@gmail.com>
2016-11-30 01:43:26 +0100

    Some refinemens were done.

--- js/basicjs.js
+++ js/basicjs.js
@@ -1,17 +1,3 @@
-window.scroll({
-  top: 2500,
-  left: 0,
-  behavior: 'smooth'
-});
-
-// Scroll certain amounts from current position
-window.scrollBy({
-  top: 100, // could be negative value
-  left: 0,
-  behavior: 'smooth'
-});
-
-// Scroll to a certain element
-document.querySelector('.ach-container').scrollIntoView({
-  behavior: 'smooth'
-});
+function scrollMe(x, y) {
+    window.scrollTo(x, y);
+}
