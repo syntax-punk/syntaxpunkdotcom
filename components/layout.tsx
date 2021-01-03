8e10a2ef17980487a9e1f31895a3840867107c57
Voogie <vugar.adbulzada@gmail.com>
2021-01-03 13:02:56 +0100

    Added some styles

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,10 +1,7 @@
 import Head from 'next/head'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
 import Header from './header'
 
-export const name = 'Voooogie dot dev'
+export const name = 'Voogie dot dev'
 export const siteTitle = 'Welcome to Voogie dot dev'
 
 export default function Layout({ children }: {
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,65 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Date from '../components/date'
 import Layout, { name } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
 
-export default function Home({ allPosts }
-  : {
-    allPosts: {
-      date: string
-      title: string
-      id: string
-    }[]
-  }) {
+const Home = () => {
   return (
     <Layout>
       <Head>
         <title>{name}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Voooooogie gone north</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      <section className="container">
+        <div className="frame">
+          <div className="banner">
+            <img src="/images/heivoogie.svg" alt="Hello logo"/>
+            <p>developer / observer / thinker</p>
+          </div>
+          <img className="photo" src="/images/me_am.png" alt="Photo of me"/>
+        </div>
       </section>
+      
+      <style jsx>{`
+        .container {
+          width: 100%;
+          height: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .frame {
+          position: relative;
+        }
+
+        .banner {
+          top: -10rem;
+          right: -12rem;
+          position: absolute;
+          z-index: 10;
+        }
+
+        .banner > p {
+          position: absolute;
+          bottom: -1.2rem;
+          right: 0;
+          color: #0000005d;
+          font-weight: 300;
+          font-size: 0.6rem;
+          text-transform: uppercase;
+        }
+
+        .photo {
+          border-radius: 4px;
+          filter: grayscale(100%);
+          transition: 600ms;
+        }
+
+        .photo:hover {
+          filter: none;
+        }
+      `}</style>
     </Layout>
   )
 }
+
+export default Home;
