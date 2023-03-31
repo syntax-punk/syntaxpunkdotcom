d75015c7cd8a464a7f51c9c8e82d3c2ab9e58aa0
theVoogie <david.jaeren@webstep.no>
2023-03-31 16:46:06 +0200

    Added some more styling for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,31 +10,37 @@ const Home = () => {
         <title>{name}</title>
       </Head>
       <Container className="fade-in three">
-        <div className="frame">
-          <article>
-              <span>
-                <span role="img" aria-label="wave">👋</span> I&apos;m David! I&apos;m a software developer from Stavanger, Norway.
-                I&apos;m passionate about building awesome products and I&apos;m always looking for new challenges. Go check out my latest projects, blog posts and other
-                stuff I&apos;ve been working on.
-                <br/>
-                <br/>
-                I&apos;m currently working at <a href="https://www.webstep.no/" className='slug' target="_blank" rel="noreferrer">WEBSTEP</a> and 
-                you can reach out to me by one of the means below!
-              </span>
-              <div className="icons-row">
-                <a href="mailto:david.jaeren@gmail.com" target="_blank" rel="noreferrer">
-                  <Image loading="eager" src="/images/email.svg" alt="Github logo" width="64" height="64px"/>
-                </a>
-                <a href="https://github.com/syntax-punk" target="_blank" rel="noreferrer">
-                  <Image loading="eager" src="/images/github.svg" alt="Github logo" width="64px" height="64px"/>
-                </a>
-                <a href="https://twitter.com/syntax_punk" target="_blank" rel="noreferrer">
-                  <Image loading="eager" src="/images/twitter.svg" alt="Twitter logo" width="64px" height="64px"/>
-                </a>
-              </div>
-          </article>
-          <div className="photo">
-            <Image loading="lazy" style={{ borderRadius: "8px" }} src="/images/david.jpg" alt="Photo of me" width="320px" height="320px" />
+        <div className="flipcard">
+          <div className="flipcard-content">
+            <figure className="flipcard-face">
+              <Image loading='lazy' src="/images/syntaxpnk.png" alt="Photo of me" width="800px" height="1000px"  />
+            </figure>
+            <div className="flipcard-back">
+              <article>
+                <span>
+                  <span role="img" aria-label="wave">👋</span> I&apos;m David! 
+                  <br/>
+                  I&apos;m a software developer from Stavanger, Norway.
+                  I&apos;m passionate about building awesome products and I&apos;m always looking for new challenges. Go check out my latest projects, blog posts and other
+                  stuff I&apos;ve been working on.
+                  <br/>
+                  <br/>
+                  I&apos;m currently working at <a href="https://www.webstep.no/" className='slug' target="_blank" rel="noreferrer">WEBSTEP</a> and 
+                  you can reach out to me by one of the means below!
+                </span>
+                <div className="icons-row">
+                  <a href="mailto:david.jaeren@gmail.com" target="_blank" rel="noreferrer">
+                    <Image loading="eager" src="/images/email.svg" alt="Github logo" width="64" height="64px"/>
+                  </a>
+                  <a href="https://github.com/syntax-punk" target="_blank" rel="noreferrer">
+                    <Image loading="eager" src="/images/github.svg" alt="Github logo" width="64px" height="64px"/>
+                  </a>
+                  <a href="https://twitter.com/syntax_punk" target="_blank" rel="noreferrer">
+                    <Image loading="eager" src="/images/twitter.svg" alt="Twitter logo" width="64px" height="64px"/>
+                  </a>
+                </div>
+            </article>
+            </div>
           </div>
         </div>
       </Container>
@@ -49,38 +55,75 @@ const Container = styled.section`
   justify-content: center;
   align-items: center;
 
-  .frame {
+  .flipcard {
+    height: 1000px;
+    width: 800px;
+    perspective: 2500px;
+    background: transparent;
+    cursor: pointer;
+  }
+
+  .flipcard-content {
+    height: 100%;
+    width: 100%;
     position: relative;
-    display: grid;
-    grid-template-columns: 2fr 1fr;
-    align-content: center;
-    align-items: stretch;
-    grid-gap: 1.6rem;
-    padding: 1rem;
-    width: 80%;
-    height: 60%;
-    min-height: 512px; 
-    border-radius: 4px;
+    text-align: center;
+    transition: transform 650ms cubic-bezier(0.68, -0.55, 0.265, 1.55);
+    transform-style: preserve-3d;
+    box-shadow: 0 0 16px 10px rgba(0, 0, 0, 0.2);
+    background-image: linear-gradient(135deg, rgba(29, 29, 29, 0.05) 0%, rgba(29, 29, 29, 0.05) 17%, rgba(27, 27, 27, 0.05) 17%, rgba(27, 27, 27, 0.05) 34%, rgba(31, 31, 31, 0.05) 34%, rgba(31, 31, 31, 0.05) 93%, rgba(242, 242, 242, 0.05) 93%, rgba(242, 242, 242, 0.05) 100%), linear-gradient(135deg, rgba(129, 129, 129, 0.05) 0%, rgba(129, 129, 129, 0.05) 66%, rgba(117, 117, 117, 0.05) 66%, rgba(117, 117, 117, 0.05) 91%, rgba(199, 199, 199, 0.05) 91%, rgba(199, 199, 199, 0.05) 100%), linear-gradient(135deg, rgba(31, 31, 31, 0.07) 0%, rgba(31, 31, 31, 0.07) 15%, rgba(139, 139, 139, 0.07) 15%, rgba(139, 139, 139, 0.07) 23%, rgba(200, 200, 200, 0.07) 23%, rgba(200, 200, 200, 0.07) 29%, rgba(102, 102, 102, 0.07) 29%, rgba(102, 102, 102, 0.07) 100%), linear-gradient(90deg, rgb(19, 196, 228), rgb(126, 8, 222));
+  }
+
+  .flipcard:hover .flipcard-content {
+    height: 100%;
+    width: 100%;
+    transform: rotateX(180deg);
+  }
+
+  .flipcard-face, .flipcard-back {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    border-radius: 2px;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+  }
+  
+  .flipcard-back {
+    transform: rotateX(180deg);
   }
   
   article {
+    display: grid;
+    padding: 2rem;
+    grid-template-rows: 80% 20%;
+    grid-row-gap: 2rem;
+    height: 100%;
     width: 100%;
+    color: #F1E9FB;
+    text-align: right;
+    
     > span {
-       background-color: #1c1b1baa;
-      font-size: 1.5em;
-      color: #f7f7f7e0;
-      line-height: 2.2rem;
-      font-weight: 300;
+      padding: 2rem;
+      width: 100%;
+      height: 100%;
+      border-radius: 10px;
+      font-size: 1.6rem;
+      line-height: 2.6rem;
+      font-weight: 400;
       letter-spacing: 0.05rem;
+      text-shadow: 0 0 12px #000;
+      background-color: #13072261;
     }
 
     .icons-row {
       display: grid;
-      grid-template-columns: 2.2rem 2rem 2rem 2rem;
+      grid-template-columns: 2.4rem 2rem 2rem;
       grid-gap: 1rem;
-      justify-content: canter;
+      justify-content: end;
       align-items: center;
-      margin-top: 0.5rem;
       width: 100%;
       display: grid;
       height: 52px;
@@ -97,7 +140,9 @@ const Container = styled.section`
 
     .slug {
       text-decoration: none;
-      color: #f7f7f7e0;
+      color: #f7f7f7;
+      font-size: 1.8rem;
+      transition: color 250ms ease-in-out;
 
       &:hover {
         color: #d082ff;
