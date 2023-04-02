e5f147d4cbef213bf3a1ecc06e207f89c1f7d55a
theVoogie <david.jaeren@webstep.no>
2023-04-02 19:59:41 +0200

    Moving background animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,6 +72,8 @@ const Container = styled.section`
     transform-style: preserve-3d;
     box-shadow: 0 0 16px 10px rgba(0, 0, 0, 0.2);
     background-image: linear-gradient(135deg, rgba(29, 29, 29, 0.05) 0%, rgba(29, 29, 29, 0.05) 17%, rgba(27, 27, 27, 0.05) 17%, rgba(27, 27, 27, 0.05) 34%, rgba(31, 31, 31, 0.05) 34%, rgba(31, 31, 31, 0.05) 93%, rgba(242, 242, 242, 0.05) 93%, rgba(242, 242, 242, 0.05) 100%), linear-gradient(135deg, rgba(129, 129, 129, 0.05) 0%, rgba(129, 129, 129, 0.05) 66%, rgba(117, 117, 117, 0.05) 66%, rgba(117, 117, 117, 0.05) 91%, rgba(199, 199, 199, 0.05) 91%, rgba(199, 199, 199, 0.05) 100%), linear-gradient(135deg, rgba(31, 31, 31, 0.07) 0%, rgba(31, 31, 31, 0.07) 15%, rgba(139, 139, 139, 0.07) 15%, rgba(139, 139, 139, 0.07) 23%, rgba(200, 200, 200, 0.07) 23%, rgba(200, 200, 200, 0.07) 29%, rgba(102, 102, 102, 0.07) 29%, rgba(102, 102, 102, 0.07) 100%), linear-gradient(90deg, rgb(19, 196, 228), rgb(126, 8, 222));
+    background-size: 200% 200%;
+	  animation: gradient 15s ease-in-out infinite;
   }
 
   .flipcard:hover .flipcard-content {
