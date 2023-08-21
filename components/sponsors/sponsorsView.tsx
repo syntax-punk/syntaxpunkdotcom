a4daef5ec6e1195ab1a032d90e62d362972fedc6
theVoogie <david.jaeren@webstep.no>
2023-08-21 16:20:23 +0200

    Button got a bit of a new style

--- components/sponsors/sponsorsView.tsx
+++ components/sponsors/sponsorsView.tsx
@@ -41,21 +41,26 @@ const Banner = styled.div`
 
 const BuyMeACoffee = styled.a`
   display: inline-block;
-  margin-top: 1rem;
-  padding: 0.1rem 1rem;
-  font-size: 2rem;
+  margin-top: 2rem;
+  padding: 0.5rem 1.25rem;
+  font-size: 1.8rem;
   font-weight: 400;
   text-decoration: none;
   color: #000000cc;
-  background-color: #65e0eb1f;
-  border-radius: 0.5rem;
+  background-color: #ffffff;
+  border: 4px solid #0894e2;
+  /* background-color: #65e0eb1f; */
   box-shadow: 0px 3px 4px 0px rgba(0, 0, 0, 0.12), 0px 2px 4px 0px rgba(0, 0, 0, 0.14);
 
   &:hover {
     color: #000000;
     text-decoration: none;
-    border: 2px groove #a9a9a94c;
     box-shadow: none;
+  }
+
+  &:active {
+    border: 4px solid #a9a9a94c;
+    background-color: #eef7fb;
   }
 `
 
