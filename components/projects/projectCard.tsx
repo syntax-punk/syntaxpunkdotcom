49b68a2ad46fccc37faa19d6e343a90613d5bf53
syntax-punk <david.jaeren@webstep.no>
2024-01-04 16:52:32 +0100

    Added a bit more styling to project cards

--- components/projects/projectCard.tsx
+++ components/projects/projectCard.tsx
@@ -42,7 +42,8 @@ const Container = styled.a`
   grid-template-rows: 200px 1.5rem min-content;
   border-radius: 4px;
   background-color: #fff;
-  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  box-shadow: var(--project-card-shadow);
+  border: var(--project-card-border);
   color: #171515;
   text-decoration: none;
 
