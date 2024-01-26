dc87918a24fe3962eacb8f4563f04285f5e71945
syntax-punk <david.jaeren@gmail.com>
2024-01-26 11:58:00 +0100

    Added plausible2

--- components/layout.tsx
+++ components/layout.tsx
@@ -22,6 +22,7 @@ export default function Layout({ children }: {
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
+        <script defer data-domain="syntaxpunk.com" src="https://plausible.io/js/script.js"></script>
       </Head>
       <main>
         <Header />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,12 @@
 /* eslint-disable @next/next/inline-script-id */
 import React from 'react'
 import { AppProps } from 'next/app'
-import { GoogleAnalytics } from '@next/third-parties/google'
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENTID} />
       <Component {...pageProps} />
     </>
   )
